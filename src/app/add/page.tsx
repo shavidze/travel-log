cc3a5093ccc1e5662670f8d8ff4753480227be39
Saba Shavidze <sabashavidze@gmail.com>
2023-04-20 17:15:06 +0400

    fix sm issue: add page

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -7,6 +7,9 @@ const Add = () => {
         onComplete={() => {
           console.log('added');
         }}
+        onCancel={() => {
+          console.error('aqs');
+        }}
       />
     </div>
   );
