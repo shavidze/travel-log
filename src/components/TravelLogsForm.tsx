8079d6a5c88d5de019a0346a9ffa35d5bb6dfbb9
Saba Shavidze <sabashavidze@gmail.com>
2023-05-02 16:05:47 +0400

    Zod types and serialization

--- src/components/TravelLogsForm.tsx
+++ src/components/TravelLogsForm.tsx
@@ -86,7 +86,6 @@ const TravelLogsForm: FC<Props> = ({ onComplete, onCancel }) => {
           Accept: 'application/json',
         },
         body: JSON.stringify(data),
-        next: { revalidate: 60 },
       });
 
       if (response.ok) {
